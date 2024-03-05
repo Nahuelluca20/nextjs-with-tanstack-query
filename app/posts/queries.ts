4544bf5afbebba313fc4123d610a8b82a418ccb7
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-05 17:49:46 -0300

    Refactor post queries and add error handling

--- app/posts/queries.ts
+++ app/posts/queries.ts
@@ -18,16 +18,20 @@ export async function getPosts() {
 
 export const createPost = action(postSchema, async ({title, content}) => {
   let id = uuidv4();
-
-  await sql`
+  const newPost = await sql`
     INSERT INTO posts (id, content, published, title) VALUES (${id}, ${content}, false, ${title})
   `;
 
   revalidatePath("/posts");
+
+  if (!newPost) return {error: "Could not create post"};
+  if (newPost) return {success: "Post Created"};
 });
 
 export const deletePost = action(deletePostSchema, async ({id}) => {
-  await sql`DELETE FROM posts WHERE id = ${id}`;
+  const deletedPost = await sql`DELETE FROM posts WHERE id = ${id}`;
 
   revalidatePath("/posts");
+  if (!deletedPost) return {error: "Could not delete post"};
+  if (deletedPost) return {success: "Post Deleted"};
 });
